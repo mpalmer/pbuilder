0556a7f7358926e6a322dd1bab7f9a07f8bae0c5
dancer <dancer>
2003-09-18 22:20:11 +0000

    update

--- testsuite/run-test.sh
+++ testsuite/run-test.sh
@@ -4,6 +4,13 @@ PBUILDER_UML=/usr/bin/pbuilder-user-mode-linux
 
 if [ -x "${PBUILDER_UML}" ]; then
     pbuilder-user-mode-linux create --distribution sid --uml-image $(pwd)/testimage --logfile pbuilder-user-mode-linux-create.log
+    ( 
+	mkdir testbuild
+	cd testbuild
+	apt-get source -d dsh
+    )
+    pbuilder-user-mode-linux build --uml-image $(pwd)/testimage --buildplace $(pwd)/testbuild/ --logfile pbuilder-user-mode-linux-build-dsh.log testbuild/dsh*.dsc
+    rm -rf testbuild
 fi
 
 
