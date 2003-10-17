744832981cbdd2f954b4239f6fba10b4e0f25fd9
dancer <dancer>
2003-10-17 23:34:13 +0000

    * pbuilder-user-mode-linux.1: update doc.
    	* pbuilder-user-mode-linux (UML_DEBUGMODE): error check if BUILDPLACE
    	is user-accessible.

--- testsuite/run-test.sh
+++ testsuite/run-test.sh
@@ -4,7 +4,7 @@ PBUILDER_UML=/usr/bin/pbuilder-user-mode-linux
 
 if [ -x "${PBUILDER_UML}" ]; then
     for distribution in sid sarge; do
-	pbuilder-user-mode-linux create --mirror http://ftp.jp.debian.org/debian/ --distribution "${distribution}" --uml-image $(pwd)/testimage --logfile pbuilder-user-mode-linux-create-${distribution}.log
+	pbuilder-user-mode-linux create --mirror  http://ring.asahi-net.or.jp/archives/linux/debian/debian --distribution "${distribution}" --uml-image $(pwd)/testimage --logfile pbuilder-user-mode-linux-create-${distribution}.log
 	( 
 	    mkdir testbuild
 	    cd testbuild
