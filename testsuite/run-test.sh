d32ca915a3d5b5679073a97a1bdbd2d880b3703a
dancer <dancer>
2003-10-18 00:21:19 +0000

    +	* testsuite/run-test.sh (PBUILDER_UML): add more cases, test-build debootstrap also. + +	* pbuilder-modules (pbuilder-options): change policy-rc.d to allow makedev +	to run. +

--- testsuite/run-test.sh
+++ testsuite/run-test.sh
@@ -5,12 +5,15 @@ PBUILDER_UML=/usr/bin/pbuilder-user-mode-linux
 if [ -x "${PBUILDER_UML}" ]; then
     for distribution in sid sarge; do
 	pbuilder-user-mode-linux create --mirror  http://ring.asahi-net.or.jp/archives/linux/debian/debian --distribution "${distribution}" --uml-image $(pwd)/testimage --logfile pbuilder-user-mode-linux-create-${distribution}.log
-	( 
-	    mkdir testbuild
-	    cd testbuild
-	    apt-get source -d dsh
-	)
-	pbuilder-user-mode-linux build --uml-image $(pwd)/testimage --buildplace $(pwd)/testbuild/ --logfile pbuilder-user-mode-linux-build-dsh-${distribution}.log testbuild/dsh*.dsc
+
+	for PKG in dsh debootstrap; do 
+	    ( 
+		mkdir testbuild
+		cd testbuild
+		apt-get source -d ${PKG}
+	    )
+	    pbuilder-user-mode-linux build --uml-image $(pwd)/testimage --buildplace $(pwd)/testbuild/ --logfile pbuilder-user-mode-linux-build-dsh-${distribution}.log testbuild/${PKG}*.dsc
+	done
 	rm -rf testbuild testimage
     done
 fi
