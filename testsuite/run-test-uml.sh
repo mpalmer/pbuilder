af3bb651833765c2070d80695c87bb31dc6a36b8
dancer <dancer>
2004-01-12 01:11:25 +0000

    move logs to testsuite/uml,normal, instead of populating testsuite dir with all files.

--- testsuite/run-test-uml.sh
+++ testsuite/run-test-uml.sh
@@ -8,7 +8,7 @@ pbuilder-user-mode-linux test --configfile non-existing-configfile
 
 if [ -x "${PBUILDER_UML}" ]; then
     for distribution in sid sarge; do
-	pbuilder-user-mode-linux create --mirror  http://ring.asahi-net.or.jp/archives/linux/debian/debian --distribution "${distribution}" --uml-image $(pwd)/testimage --logfile pbuilder-user-mode-linux-create-${distribution}.log
+	pbuilder-user-mode-linux create --mirror  http://ring.asahi-net.or.jp/archives/linux/debian/debian --distribution "${distribution}" --uml-image $(pwd)/testimage --logfile uml/pbuilder-user-mode-linux-create-${distribution}.log
 
 	for PKG in dsh; do 
 	    ( 
@@ -16,17 +16,17 @@ if [ -x "${PBUILDER_UML}" ]; then
 		cd testbuild
 		apt-get source -d ${PKG}
 	    )
-	    pbuilder-user-mode-linux build --uml-image $(pwd)/testimage --buildplace $(pwd)/testbuild/ --logfile pbuilder-user-mode-linux-build-${PKG}-${distribution}.log testbuild/${PKG}*.dsc
+	    pbuilder-user-mode-linux build --uml-image $(pwd)/testimage --buildplace $(pwd)/testbuild/ --logfile uml/pbuilder-user-mode-linux-build-${PKG}-${distribution}.log testbuild/${PKG}*.dsc
 	    (
 		mkdir testbuild2
 		cd testbuild2
 		apt-get source ${PKG}
 		cd ${PKG}-*
-		pdebuild-user-mode-linux --logfile ../../pdebuild-user-mode-linux-normal-${distribution}.log -- --uml-image $(pwd)/../../testimage --buildplace $(pwd)/../../testbuild2/
-		pdebuild-user-mode-linux --use-pdebuild-internal --logfile ../../pdebuild-user-mode-linux-internal-${distribution}.log -- --uml-image $(pwd)/../../testimage --buildplace $(pwd)/../../testbuild2/
+		pdebuild-user-mode-linux --logfile ../../uml/pdebuild-user-mode-linux-normal-${distribution}.log -- --uml-image $(pwd)/../../testimage --buildplace $(pwd)/../../testbuild2/
+		pdebuild-user-mode-linux --use-pdebuild-internal --logfile ../../uml/pdebuild-user-mode-linux-internal-${distribution}.log -- --uml-image $(pwd)/../../testimage --buildplace $(pwd)/../../testbuild2/
 	    )
 	done
-	pbuilder-user-mode-linux execute --uml-image $(pwd)/testimage --logfile pbuilder-user-mode-linux-execute-${distribution}.log ../examples/execute_paramtest.sh test1 test2 test3
+	pbuilder-user-mode-linux execute --uml-image $(pwd)/testimage --logfile uml/pbuilder-user-mode-linux-execute-${distribution}.log ../examples/execute_paramtest.sh test1 test2 test3
 	rm -rf testbuild testbuild2 testimage
     done
 fi
--- testsuite/run-test.sh
+++ testsuite/run-test.sh
@@ -4,14 +4,14 @@
 
 PBUILDER=/usr/sbin/pbuilder
 
-vmstat -n 1 > vmstat &
+vmstat -n 1 > normal/vmstat &
 VMSTATPID=$!
-LC_ALL=C iostat -dxt hda 1 > iostat & 
+LC_ALL=C iostat -dxt hda 1 > normal/iostat & 
 IOSTATPID=$!
 
 if [ -x "${PBUILDER}" ]; then
     for distribution in sid sarge; do
-	sudo ${PBUILDER} create --mirror http://ring.asahi-net.or.jp/archives/linux/debian/debian --distribution "${distribution}" --basetgz $(pwd)/testimage --logfile pbuilder-create-${distribution}.log 
+	sudo ${PBUILDER} create --mirror http://ring.asahi-net.or.jp/archives/linux/debian/debian --distribution "${distribution}" --basetgz $(pwd)/testimage --logfile normal/pbuilder-create-${distribution}.log 
 
 	for PKG in dsh; do 
 	    ( 
@@ -19,17 +19,17 @@ if [ -x "${PBUILDER}" ]; then
 		cd testbuild
 		apt-get source -d ${PKG}
 	    )
-	    sudo ${PBUILDER} build --basetgz $(pwd)/testimage --buildplace $(pwd)/testbuild/ --logfile pbuilder-build-${PKG}-${distribution}.log testbuild/${PKG}*.dsc
+	    sudo ${PBUILDER} build --basetgz $(pwd)/testimage --buildplace $(pwd)/testbuild/ --logfile normal/pbuilder-build-${PKG}-${distribution}.log testbuild/${PKG}*.dsc
 	    (
 		mkdir testbuild2
 		cd testbuild2
 		apt-get source ${PKG}
 		cd ${PKG}-*
-		pdebuild --logfile ../../pdebuild-normal-${distribution}.log -- --basetgz $(pwd)/../../testimage --buildplace $(pwd)/../../testbuild2/
-		pdebuild --use-pdebuild-internal --logfile ../../pdebuild-internal-${distribution}.log -- --basetgz $(pwd)/../../testimage --buildplace $(pwd)/../../testbuild2/
+		pdebuild --logfile ../../normal/pdebuild-normal-${distribution}.log -- --basetgz $(pwd)/../../testimage --buildplace $(pwd)/../../testbuild2/
+		pdebuild --use-pdebuild-internal --logfile ../../normal/pdebuild-internal-${distribution}.log -- --basetgz $(pwd)/../../testimage --buildplace $(pwd)/../../testbuild2/
 	    )
 	done
-	sudo ${PBUILDER} execute --basetgz $(pwd)/testimage --logfile pbuilder-execute-${distribution}.log ../examples/execute_paramtest.sh test1 test2 test3
+	sudo ${PBUILDER} execute --basetgz $(pwd)/testimage --logfile normal/pbuilder-execute-${distribution}.log ../examples/execute_paramtest.sh test1 test2 test3
 	sudo rm -rf testbuild testbuild2 testimage
     done
 fi
