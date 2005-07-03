5f5f86b55f4d79a464e04d263c42af3f0cd36a73
dancer <dancer>
2005-07-03 11:58:59 +0000

    +	* testsuite/run-test.sh (RESULTFILE): remove vmstat and iostat. I don't really need them; +	and add test for etch.

--- testsuite/run-test.sh
+++ testsuite/run-test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a testsuite.
-# estimated run-time on my PC; 11 minutes.
+# estimated run-time on my PC; 16 minutes.
 
 PBUILDER=/usr/sbin/pbuilder
 
@@ -17,45 +17,65 @@ log_success () {
     fi
 }
 
-vmstat -n 1 > normal/vmstat &
-VMSTATPID=$!
-LC_ALL=C iostat -dxt hda 1 > normal/iostat & 
-IOSTATPID=$!
+
+
+mirror=http://ring.asahi-net.or.jp/archives/linux/debian/debian
+logdir=$(readlink -f normal/)
+
+testdir=$(TMPDIR=$(pwd) mktemp -d)
+testimage=$testdir/testimage
+testbuild=$testdir/dir1
+testbuild2=$testdir/dir2
 
 if [ -x "${PBUILDER}" ]; then
-    for distribution in sid sarge; do
-	sudo ${PBUILDER} create --mirror http://ring.asahi-net.or.jp/archives/linux/debian/debian --distribution "${distribution}" --basetgz $(pwd)/testimage --logfile normal/pbuilder-create-${distribution}.log 
+    for distribution in sid sarge etch; do
+	sudo ${PBUILDER} create --mirror $mirror --distribution "${distribution}" --basetgz ${testimage} --logfile ${logdir}/pbuilder-create-${distribution}.log 
 # --hookdir /usr/share/doc/pbuilder/examples/libc6workaround
 	log_success create-${distribution}
 
 	for PKG in dsh; do 
 	    ( 
-		mkdir testbuild
-		cd testbuild
+		mkdir ${testbuild}
+		cd ${testbuild}
 		apt-get source -d ${PKG}
 	    )
-	    sudo ${PBUILDER} build --debemail "Junichi Uekawa <dancer@debian.org>" --basetgz $(pwd)/testimage --buildplace $(pwd)/testbuild/ --logfile normal/pbuilder-build-${PKG}-${distribution}.log testbuild/${PKG}*.dsc
+	    sudo ${PBUILDER} build --debemail "Junichi Uekawa <dancer@debian.org>" --basetgz ${testimage} --buildplace ${testbuild}/ --logfile ${logdir}/pbuilder-build-${PKG}-${distribution}.log ${testbuild}/${PKG}*.dsc
 	    log_success build-${distribution}-${PKG}
 
 	    (
-		mkdir testbuild2
-		cd testbuild2
+		mkdir ${testbuild2}
+		cd ${testbuild2}
 		apt-get source ${PKG}
 		cd ${PKG}-*
-		pdebuild --logfile ../../normal/pdebuild-normal-${distribution}.log -- --basetgz $(pwd)/../../testimage --buildplace $(pwd)/../../testbuild2/
+		pdebuild --logfile ${logdir}/pdebuild-normal-${distribution}.log -- --basetgz ${testimage} --buildplace ${testbuild2}
 		log_success pdebuild-${distribution}-${PKG}
 
-		pdebuild --use-pdebuild-internal --logfile ../../normal/pdebuild-internal-${distribution}.log -- --basetgz $(pwd)/../../testimage --buildplace $(pwd)/../../testbuild2/
+		pdebuild --use-pdebuild-internal --logfile ${logdir}/pdebuild-internal-${distribution}.log -- --basetgz ${testimage} --buildplace ${testbuild2}
 		log_success pdebuild-internal-${distribution}-${PKG}
-
 	    )
 	done
-	sudo ${PBUILDER} execute --basetgz $(pwd)/testimage --logfile normal/pbuilder-execute-${distribution}.log ../examples/execute_paramtest.sh test1 test2 test3
-	log_success execute-${distribution}
+	sudo ${PBUILDER} execute --basetgz ${testimage} --logfile ${logdir}/pbuilder-execute-${distribution}.log ../examples/execute_paramtest.sh test1 test2 test3
 
-	sudo rm -rf testbuild testbuild2 testimage
+	# upgrading testing.
+	case $distribution in 
+	    sarge)
+		sudo ${PBUILDER} update --basetgz ${testimage} --distribution etch --mirror $mirror --override-config --logfile ${logdir}/pbuilder-update-${distribution}-etch.log 
+		log_success update-${distribution}-etch.log
+		sudo ${PBUILDER} update --basetgz ${testimage} --distribution sid --mirror $mirror --override-config --logfile ${logdir}/pbuilder-update-${distribution}-etch-sid.log 
+		log_success update-${distribution}-etch-sid.log
+		sudo ${PBUILDER} update --basetgz ${testimage} --distribution experimental --mirror $mirror --override-config --logfile ${logdir}/pbuilder-update-${distribution}-etch-sid-experimental.log 
+		log_success update-${distribution}-etch-sid-experimental.log
+		;;
+	    etch)
+		sudo ${PBUILDER} update --basetgz ${testimage} --distribution sid --mirror $mirror --override-config --logfile ${logdir}/pbuilder-update-${distribution}-sid.log 
+		log_success update-${distribution}-sid.log
+		sudo ${PBUILDER} update --basetgz ${testimage} --distribution experimental --mirror $mirror --override-config --logfile ${logdir}/pbuilder-update-${distribution}-sid-experimental.log 
+		log_success update-${distribution}-sid-experimental.log
+		;;
+	esac
+
+	sudo rm -rf ${testbuild} ${testbuild2} ${testimage}
     done
 fi
 
-kill $VMSTATPID
-kill $IOSTATPID
+rm -r ${testdir}
