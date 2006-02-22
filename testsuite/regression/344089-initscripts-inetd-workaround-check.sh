f3a31e42bee44bf0471e08613fa0729486705bed
dancer <dancer>
2006-02-22 15:13:33 +0000

    update testsuite results, all successful.
    remove testsuite/regression/344089-initscripts-inetd-workaround-check.sh, since the problem is now gone.

--- testsuite/regression/344089-initscripts-inetd-workaround-check.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#make sure that workaround for #344089, and #262627 work.
-
-sudo cp /var/cache/pbuilder/base-sarge.tgz work/base-unstable.tgz
-sudo pbuilder update  --logfile "$OUTPUT_LOGNAME" --distribution unstable --basetgz work/base-unstable.tgz --hookdir /usr/share/doc/pbuilder/examples/344089 --override-config
-
