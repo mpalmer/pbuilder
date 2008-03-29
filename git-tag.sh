c77e778122f574f2a067e6414537ffa5bdb5397b
Junichi Uekawa <dancer@netfort.gr.jp>
2008-03-29 16:03:30 +0900

    annotate how I manage git-dch here so that I don't forget.

--- git-tag.sh
+++ git-tag.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# run this script after doing git-dch -R --since debian/0.xxx and
+# massaging the changelog a bit.
+
 VERSION=$(dpkg-parsechangelog | sed -n 's/^Version: //p')
 git-tag -s -u dancer@debian.org -m "pbuilder release $VERSION" debian/$VERSION
 
